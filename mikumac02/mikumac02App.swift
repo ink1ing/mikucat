936c5100b328bbb9c6e1a927ba8f1259af6d25e5
ink1ing <ink1ing@example.com>
2025-08-31 18:26:13 +0800

    Initial Commit

---
+++ mikumac02/mikumac02App.swift
@@ -0,0 +1,17 @@
+//
+//  mikumac02App.swift
+//  mikumac02
+//
+//  Created by INKLING on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct mikumac02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
